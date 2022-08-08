3e319a9b8ba9b0e6758950adf99d05b3be088436
manuelsrleon <manuelsrleon@gmail.com>
2022-08-08 23:34:15 +0200

    added some transitions

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-import * as THREE from 'three.min.js';
+let listItems = [...document.querySelectorAll('h1')];
 
-const scene = new THREE.Scene(); 
+let options = {
+    rootMargin: '-10%',
+    threshold: 0.0
+}
+
+function hide(entries){
+    listItems.forEach(item => item.style.opacity = "0");
+}
+hide(listItems.entries);
+
+let observer = new IntersectionObserver(makeVisible, options);
+
+function makeVisible(entries){
+    entries.forEach(entry => {
+        console.log(entries);
+        if(entry.isIntersecting){
+            entry.target.classList.add('appearing');
+        } 
+    })
+}
+
+listItems.forEach(item => {
+    observer.observe(item);
+})
